a4a7c6fdcdc034fbb6bf6b3c14d43de189397492
rsosseh <ray_sosseh@brown.edu>
2017-06-25 19:52:50 -0400

    a

--- server.js
+++ server.js
@@ -77,7 +77,7 @@ io.sockets.on('connection', function(socket){
 	});
 
 	socket.on('message', function(message, roomName, nickname){
-		conn.query('INSERT into messages VALUES ($1, $2, $3, $4, $5)',[0, roomName, nickname, message, 0]);
+		pool.query('INSERT into messages VALUES ($1, $2, $3, $4, $5)',[0, roomName, nickname, message, 0]);
 		io.sockets.in(roomName).emit('message', nickname, message, 0);
 
 	});
@@ -87,8 +87,8 @@ io.sockets.on('connection', function(socket){
 		io.sockets.in(socket.roomName).emit('leave', socket.nickname);
 		room_size[socket.roomName] -= 1;
 		if(room_size[socket.roomName] == 0){
-			conn.query("DELETE FROM messages WHERE room= $1",[socket.roomName]);
-			conn.query('SELECT DISTINCT room FROM messages',function(err,result){
+			pool.query("DELETE FROM messages WHERE room= $1",[socket.roomName]);
+			pool.query('SELECT DISTINCT room FROM messages',function(err,result){
 				for(var i = 0; i < result.rows.length; i++){
 					io.sockets.in(result.rows[i].room).emit('refresh', result.rows);
 						
@@ -102,7 +102,7 @@ io.sockets.on('connection', function(socket){
 
 app.get('/', function(req, res){
 
-	conn.query('SELECT DISTINCT room FROM messages',function(err,result){
+	pool.query('SELECT DISTINCT room FROM messages',function(err,result){
 		var room_name = generateRoomIdentifier();
 		// console.log(result);
 		res.render('front_page.html', {rooms: JSON.stringify(result.rows), room_name: room_name})
@@ -113,7 +113,7 @@ app.get('/', function(req, res){
 
 app.get('/:roomName', function(req, res){
 
-	conn.query('SELECT DISTINCT room FROM messages',function(err,result){
+	pool.query('SELECT DISTINCT room FROM messages',function(err,result){
 		res.render('chat_page.html', {rooms: JSON.stringify(result.rows)});
 		for(var i = 0; i < result.rows.length; i++){
 			io.sockets.in(result.rows[i].room).emit('refresh', result.rows);
@@ -132,7 +132,7 @@ function generateRoomIdentifier() {
     for (var i = 0; i < 6; i++)
         result += chars.charAt(Math.floor(Math.random() * chars.length));
 
-    if (conn.query) {};
+    if (pool.query) {};
 
     return result;
 }
