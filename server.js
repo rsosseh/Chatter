15eac0fae55bb8e09a5a7cbad785d32391614dd0
rsosseh <ray_sosseh@brown.edu>
2017-06-25 18:31:03 -0400

    new database

--- server.js
+++ server.js
@@ -11,8 +11,10 @@ app.set('views',__dirname + '/templates');
 app.set('view engine', 'html');
 
 //Database Requires
-var any_db = require('any-db');
-var conn = any_db.createConnection('sqlite3://realtime.db');
+var pg = require('pg');
+pg.defaults.ssl = true;
+var pool = new pg.Pool();
+
 
 //Static Requires
 app.use(express.static('imgs'))
@@ -20,87 +22,87 @@ app.use(express.static('imgs'))
 //creating database
 var create = 'CREATE TABLE messages (id INTEGER ,room TEXT,nickname TEXT,body TEXT, time INTERGER);';
 
-conn.query(create, function(err, res){
-	console.log('made table');
-});
-
-
 //creating realtime sockets
 var io = require('socket.io').listen(server);
 
 
-room_size = {}
-io.sockets.on('connection', function(socket){
+pool.connect(process.env.DATABASE_URL, function(err, conn){
+	if(err) throw err;
 
-	socket.on('join', function(roomName, nickname, callback){
-		socket.join(roomName);
-		socket.nickname = nickname;
-		socket.roomName = roomName;
+	room_size = {}
+	io.sockets.on('connection', function(socket){
 
-		// keep track of #of ppl in rooms to delete empty rooms
-		if (room_size[socket.roomName] == undefined) {
-			room_size[socket.roomName] = 1;
-		}
-		else{
-			room_size[socket.roomName] += 1;
-		}
+		socket.on('join', function(roomName, nickname, callback){
+			socket.join(roomName);
+			socket.nickname = nickname;
+			socket.roomName = roomName;
+
+			// keep track of #of ppl in rooms to delete empty rooms
+			if (room_size[socket.roomName] == undefined) {
+				room_size[socket.roomName] = 1;
+			}
+			else{
+				room_size[socket.roomName] += 1;
+			}
 
 
-		var clients_in_room = io.sockets.adapter.rooms[roomName];
-		var nicknames = [];
-		for (var i = 0; i < clients_in_room.length; i++){
-			nicknames[i] = io.sockets.connected[Object.keys(clients_in_room.sockets)[i]].nickname;
-		};
+			var clients_in_room = io.sockets.adapter.rooms[roomName];
+			var nicknames = [];
+			for (var i = 0; i < clients_in_room.length; i++){
+				nicknames[i] = io.sockets.connected[Object.keys(clients_in_room.sockets)[i]].nickname;
+			};
 
-		io.sockets.in(roomName).emit('membership_changed',nicknames);
+			io.sockets.in(roomName).emit('membership_changed',nicknames);
+		});
+
+		socket.on('message', function(message, roomName, nickname){
+			conn.query('INSERT into messages VALUES ($1, $2, $3, $4, $5)',[0, roomName, nickname, message, 0]);
+			io.sockets.in(roomName).emit('message', nickname, message, 0);
+
+		});
+
+
+		socket.on('disconnect', function(){
+			io.sockets.in(socket.roomName).emit('leave', socket.nickname);
+			room_size[socket.roomName] -= 1;
+			if(room_size[socket.roomName] == 0){
+				conn.query("DELETE FROM messages WHERE room= $1",[socket.roomName]);
+				conn.query('SELECT DISTINCT room FROM messages',function(err,result){
+					for(var i = 0; i < result.rows.length; i++){
+						io.sockets.in(result.rows[i].room).emit('refresh', result.rows);
+							
+					}
+				});
+			};
+		});
+
+
 	});
 
-	socket.on('message', function(message, roomName, nickname){
-		conn.query('INSERT into messages VALUES ($1, $2, $3, $4, $5)',[0, roomName, nickname, message, 0]);
-		io.sockets.in(roomName).emit('message', nickname, message, 0);
+	app.get('/', function(req, res){
+
+		conn.query('SELECT DISTINCT room FROM messages',function(err,result){
+			var room_name = generateRoomIdentifier();
+			res.render('front_page.html', {rooms: JSON.stringify(result.rows), room_name: room_name})
+		});
 
 	});
 
+	app.get('/:roomName', function(req, res){
 
-	socket.on('disconnect', function(){
-		io.sockets.in(socket.roomName).emit('leave', socket.nickname);
-		room_size[socket.roomName] -= 1;
-		if(room_size[socket.roomName] == 0){
-			conn.query("DELETE FROM messages WHERE room= $1",[socket.roomName]);
-			conn.query('SELECT DISTINCT room FROM messages',function(err,result){
-				for(var i = 0; i < result.rows.length; i++){
-					io.sockets.in(result.rows[i].room).emit('refresh', result.rows);
-						
-				}
-			});
-		};
+		conn.query('SELECT DISTINCT room FROM messages',function(err,result){
+			res.render('chat_page.html', {rooms: JSON.stringify(result.rows)});
+			for(var i = 0; i < result.rows.length; i++){
+				io.sockets.in(result.rows[i].room).emit('refresh', result.rows);
+					
+			}
+		});
+		
 	});
 
+	server.listen(process.env.PORT || 8080);
 
 });
-
-app.get('/', function(req, res){
-
-	conn.query('SELECT DISTINCT room FROM messages',function(err,result){
-		var room_name = generateRoomIdentifier();
-		res.render('front_page.html', {rooms: JSON.stringify(result.rows), room_name: room_name})
-	});
-
-});
-
-app.get('/:roomName', function(req, res){
-
-	conn.query('SELECT DISTINCT room FROM messages',function(err,result){
-		res.render('chat_page.html', {rooms: JSON.stringify(result.rows)});
-		for(var i = 0; i < result.rows.length; i++){
-			io.sockets.in(result.rows[i].room).emit('refresh', result.rows);
-				
-		}
-	});
-	
-});
-
-server.listen(process.env.PORT || 8080);
 
 function generateRoomIdentifier() {
     var chars = 'ABCDEFGHJKLMNPQRSTUVWXYZ23456789';
