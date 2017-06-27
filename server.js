037601ed93c7c43a9360633986552116aa93fa61
rsosseh <ray_sosseh@brown.edu>
2017-06-26 22:23:18 -0400

    afd

--- server.js
+++ server.js
@@ -12,7 +12,21 @@ app.set('view engine', 'html');
 
 //Database Requires
 var pg = require('pg')
-var pool = new pg.Pool({connectionString:process.env.DATABASE_URL})
+const url = require('url')
+
+const params = url.parse(process.env.DATABASE_URL);
+const auth = params.auth.split(':');
+
+const config = {
+  user: auth[0],
+  password: auth[1],
+  host: params.hostname,
+  port: params.port,
+  database: params.pathname.split('/')[1],
+  ssl: true
+};
+
+var pool = new pg.Pool(config);
 
 
 //Static Requires
